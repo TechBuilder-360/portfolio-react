d12f2bd6ee91f6f50a6e02d0a2f96e17f921a639
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-08-18 22:38:59 +0100

    Renamed hoc folder

--- src/authentication/LoginForm/LoginForm.js
+++ src/authentication/LoginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './LoginForm.module.css';
-import Layout from '../../hoc/Layout/Layout';
+import Layout from '../../HOC/Layout/Layout';
 
 const loginForm = () => {
   return (
