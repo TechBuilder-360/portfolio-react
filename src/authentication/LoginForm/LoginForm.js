d58125a76f3642ffa74e0a1b5abe74905229619c
Jubril Aminu <jubrilaminu304@gmail.com>
2020-09-07 08:10:36 +0100

    continued login page design

--- src/authentication/LoginForm/LoginForm.js
+++ src/authentication/LoginForm/LoginForm.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import classes from './LoginForm.module.css';
-import Layout from '../../container/Layout/Layout';
-
+// import Layout from '../../container/Layout/Layout';
+// className={`${classes.input} ${classes.email}`} 
 const LoginForm = () => {
   return (
     <div className={classes.Login}>
-        <div className="column">
-            <div className="xlogo">logo here</div>
-            <div className="signinbox">
-                <p className="signin_title">Sign in</p>
+        <div className={classes.column}>
+            <div className={classes.xlogo}>logo here</div>
+            <div className={classes.signinbox}>
+                <p className={classes.signin_title}>Sign in</p>
                 <div>
-                    <input value="Email"/><br/>
-                    <input value="Password"/>
+                    <input className={`${classes.input}`} type="email" placeholder="Email"/><br/>
+                    <input className={`${classes.input} ${classes.password}`}type="password" placeholder="Password"/>
                     <button>Login</button>
                     <p>Forgot password?</p>
                 </div>
             </div>
         </div>
-        <div className="column">Right</div>
+        <div className={classes.column}>Right</div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
