14d147f0a917c4a4f0262389c443ba307e72811e
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-09-18 20:57:23 +0100

    added more content to pre auth pages

---
+++ src/components/pre-auth/FAQs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FAQs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FAQs;
---
+++ src/components/pre-auth/Features.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <div>
+            <p className='title'>Features</p>
+            <ul>
+                <li>Create a portfolio.</li>
+                <li>Generate custom link to share portfolio.</li>
+                <li>Use custom theme for present portfolio.</li>
+                <li>Choose a prefered template for resume download.</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Features;
