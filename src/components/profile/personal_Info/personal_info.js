b20ab31062d2c1d87bcece3245e689e15db73fee
Jubril Aminu <jubrilaminu304@gmail.com>
2020-09-20 15:51:06 +0100

    added dashboard feature

---
+++ src/components/profile/personal_Info/personal_info.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalInfo = props => {
+    return (
+        <div>
+            <p>{props.title}</p>
+        </div>
+    )
+}
+
+export default PersonalInfo
---
+++ src/components/profile/skills/skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = props => {
+    return (
+        <div>
+            <p>{props.title}</p>
+        </div>
+    )
+}
+
+export default Skills
