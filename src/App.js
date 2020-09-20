94fa56fa8986053167298230935f0c03310e0979
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-09-20 16:39:33 +0100

    adjusted authentication UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 import Layout from './container/Layout/Layout';
 
--- src/components/Navigation/main-navigation.js
+++ src/components/Navigation/main-navigation.js
@@ -1,6 +1,5 @@
 import React from "react";
 import classes from "./main-navigation.module.css";
-import "../../fonts/Poppins-Bold.ttf";
 import logo from "../../images/logo.png";
 import { NavLink } from "react-router-dom";
 
@@ -9,24 +8,13 @@ import { NavLink } from "react-router-dom";
 const HomeNavBar = () => {
   return (
     <nav className={classes.Nav}>
-      <img src={logo} className={classes.Logo} />
+      <img src={logo} className={classes.Logo} alt="xPortfolio"/>
       <div className={classes.Nav_item}>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/features"}>Features</NavLink>
         <NavLink to={"/faqs"}>FAQs</NavLink>
         <NavLink to={"/about"}>About us</NavLink>
         <NavLink to={"/contact"}>Contact us</NavLink>
-        
-        
-        <div className="dropdown mt-2">
-          <p className="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Signin/Signup
-          </p>
-          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <NavLink className="dropdown-item" to={"/login"}>Signin</NavLink>
-            <NavLink className="dropdown-item" to={"/signup"}>Signup</NavLink>
-          </div>
-        </div>
       </div>
     </nav>
   );
