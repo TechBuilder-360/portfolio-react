d776b8f44809427aa5bda89db0a042e2068cef96
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-10-01 13:38:52 +0100

    added more skill to initial state

--- src/store/reducers/portfolioReducer.js
+++ src/store/reducers/portfolioReducer.js
@@ -13,12 +13,35 @@ const initialState = {
     nationality: "Nigeria",
     date_of_birth: "12th, December",
     profession: "Accountant",
-    profile_pix:"",
-      // "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/440px-Lion_waiting_in_Namibia.jpg",
+    profile_pix: "",
+    // "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/440px-Lion_waiting_in_Namibia.jpg",
     bio:
       "A graduate of Accounting, Ladoke Akintola University of Technology, A passionate writter and a loving Dog owner",
   },
-  skills: ["ICAN, BSc, MBA"],
+  skills: [
+    "ICAN",
+    "BSc",
+    "MBA",
+    "HTML",
+    "CSS",
+    "CCNA",
+    "AGILE",
+    "SCRUM",
+    "COMPTIA N+",
+    "COMPTIA A+",
+    "Python",
+    "Java",
+    "NodeJs",
+    "PHP",
+    "SPSS",
+    "Scala",
+    "Kafka",
+    "Cassandra",
+    "Postgres",
+    "Django",
+    "Spring Boot",
+    "Akka"
+  ],
   education: [
     {
       education_type: "Tertiary",
@@ -38,97 +61,113 @@ const initialState = {
     },
   ],
   experience: [
-      {
-          organization: "Consolidate Insurance",
-          description: "Worked as an account manager",
-          position: "Accounting managment",
-          start_year: 2016,
-          end_year: 2018,
-      },
-      {
-        organization: "Access Bank",
-        description: "Worked as an account manager",
-        position: "Accounting managment",
-        start_year: 2018,
-        end_year: null,
-    }
+    {
+      organization: "Consolidate Insurance",
+      description: "Worked as an account manager",
+      position: "Accounting managment",
+      start_year: 2016,
+      end_year: 2018,
+    },
+    {
+      organization: "Access Bank",
+      description: "Worked as an account manager",
+      position: "Accounting managment",
+      start_year: 2018,
+      end_year: null,
+    },
   ],
-  project: [{
+  project: [
+    {
       title: "Fraud detection using transaction pattern",
-      description: "Fraud detection using transaction pattern and previous account records."
-  },{
-    title: "Fraud detection using transaction pattern",
-    description: "Fraud detection using transaction pattern and previous account records."
-},
-{
-    title: "Fraud detection using transaction pattern",
-    description: "Fraud detection using transaction pattern and previous account records."
-},{
-    title: "Fraud detection using transaction pattern",
-    description: "Fraud detection using transaction pattern and previous account records."
-},{
-  title: "Fraud detection using transaction pattern",
-  description: "Fraud detection using transaction pattern and previous account records."
-},
-{
-  title: "Fraud detection using transaction pattern",
-  description: "Fraud detection using transaction pattern and previous account records."
-},{
-    title: "Fraud detection using transaction pattern",
-    description: "Fraud detection using transaction pattern and previous account records."
-},{
-  title: "Fraud detection using transaction pattern",
-  description: "Fraud detection using transaction pattern and previous account records."
-},
-{
-  title: "Fraud detection using transaction pattern",
-  description: "Fraud detection using transaction pattern and previous account records."
-},
-],
-social:[{
-  label: "facebook",
-  url: "https://facebook.com"
-},
-{
-  label: "twitter",
-  url: "https://twitter.com"
-},
-]
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+    {
+      title: "Fraud detection using transaction pattern",
+      description:
+        "Fraud detection using transaction pattern and previous account records.",
+    },
+  ],
+  social: [
+    {
+      label: "facebook",
+      url: "https://facebook.com",
+    },
+    {
+      label: "twitter",
+      url: "https://twitter.com",
+    },
+  ],
 };
 
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.PERSONAL_INFORMATION:
+      return {
+        ...state,
+      };
+    case actionType.EDUCATION:
+      return {
+        ...state,
+      };
+    case actionType.EXPERIENCE:
+      return {
+        ...state,
+      };
+    case actionType.PROFESSIONAL_SUMMARY:
+      return {
+        ...state,
+      };
+    case actionType.PROJECTS:
+      return {
+        ...state,
+      };
+    case actionType.SKILL:
+      return {
+        ...state,
+      };
+    case actionType.SOCIAL_LINK:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
 
-const reducer = (state=initialState, action) => {
-    switch(action.type){
-        case actionType.PERSONAL_INFORMATION:
-            return {
-                ...state
-            }
-        case actionType.EDUCATION:
-            return {
-                ...state
-            }
-            case actionType.EXPERIENCE:
-        return {
-            ...state
-        }
-        case actionType.PROFESSIONAL_SUMMARY:
-        return {
-            ...state
-        }
-        case actionType.PROJECTS:
-        return {
-            ...state
-        }
-        case actionType.SKILL:
-        return {
-            ...state
-        }
-        case actionType.SOCIAL_LINK:
-        return {
-            ...state
-        }
-        default: return state
-    }
-}
-
-export default reducer
+export default reducer;
