493e45bf2d5e7f754579d6f912d182354eaaf6df
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-10-02 17:27:26 +0100

    improved skill styling

--- src/components/profile/skills/skills.js
+++ src/components/profile/skills/skills.js
@@ -1,43 +1,27 @@
-import { map } from 'jquery'
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import classes from './skills.module.css'
 
-const Skills = props => {
-    let skillStyle={
-     skill:  { listStyle:'none',
-         display:'inline-block',
-         fontSize:'12pt',
-         padding:'6px' ,
-         fontFamily:'tahoma',
-        },
-         
-         header:{
-             textAlign:'center',
-             padding: '10px',
-            fontSize:'20pt'
-         }
-         
-    }
-    return (
-        <div className={props.wrapper}>
-            <h2 style={skillStyle.header}>{props.title}</h2>
-<ul>
-{props.skills.map((skills,index)=>
-<li key={index} style={skillStyle.skill}>{skills}</li>
+const Skills = (props) => {
+  
+  return (
+    <div className={props.wrapper}>
+      <p className="title" style={{textAlign: "left"}}>{props.title}</p>
+      <ul className={classes.Skill}>
+        {props.skills.map((skills, index) => (
+          <li key={index} title={skills} >
+            {skills}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-)}
+const mapStateToProps = (state) => {
+  return {
+    skills: state.skills,
+  };
+};
 
-
-</ul>
-
-        </div>
-    )
-}
-
-const mapStateToProps = state =>{
-    return{
-        skills:state.skills
-    }
-}
-
-export default connect(mapStateToProps)(Skills)
+export default connect(mapStateToProps)(Skills);
