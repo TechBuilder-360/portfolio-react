5a94157f9b4f5bbc47d517077666bb09915491f3
folayarn <folayanshola@gmail.com>
2020-10-02 15:13:24 +0100

    'part'

--- src/components/profile/skills/skills.js
+++ src/components/profile/skills/skills.js
@@ -1,10 +1,35 @@
+import { map } from 'jquery'
 import React from 'react'
 import { connect } from 'react-redux'
 
 const Skills = props => {
+    let skillStyle={
+     skill:  { listStyle:'none',
+         display:'inline-block',
+         fontSize:'12pt',
+         padding:'6px' ,
+         fontFamily:'tahoma',
+        },
+         
+         header:{
+             textAlign:'center',
+             padding: '10px',
+            fontSize:'20pt'
+         }
+         
+    }
     return (
         <div className={props.wrapper}>
-            <p>{props.title}</p>
+            <h2 style={skillStyle.header}>{props.title}</h2>
+<ul>
+{props.skills.map((skills,index)=>
+<li key={index} style={skillStyle.skill}>{skills}</li>
+
+)}
+
+
+</ul>
+
         </div>
     )
 }
