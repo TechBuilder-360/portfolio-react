645fec2671f4e4f49931a7c14da5e855fc8a0ad1
Jubril Aminu <jubrilaminu304@gmail.com>
2020-10-04 08:33:32 +0100

    fixed issue with year ordering

--- src/components/profile/education/education.js
+++ src/components/profile/education/education.js
@@ -1,63 +1,77 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import classes from './education.module.css'
+import React from "react";
+import { connect } from "react-redux";
+import classes from "./education.module.css";
 
-const Education = props => {
+const Education = (props) => {
+  let direction = classes.left;
+  let educationTypeContent = null;
 
-    let direction = classes.left
-    let educationTypeContent = null
+  const display = (education) => {
+    switch (education.education_type) {
+      case "Tertiary":
+        educationTypeContent = (
+          <div className={classes.content}>
+            <h2>
+              {education.end_year
+                ? `${education.start_year} - ${education.end_year}`
+                : "Till Date"}
+            </h2>
+            <p>Institution: {education.institution}</p>
+            <p>Course of study: {education.course}</p>
+            <p>Degree Obtained: {education.degree}</p>
+            <p>Class of Degree: {education.class_of_degree}</p>
+          </div>
+        );
+        break;
 
-    const display = (education) => {
+      case "College":
+        educationTypeContent = (
+          <div className={classes.content}>
+            <h2>
+              {education.end_year
+                ? `${education.start_year} - ${education.end_year}`
+                : `${education.start_year} - Till Date`}
+            </h2>
+            <p>Institution: {education.institution}</p>
+            <p>Degree Obtained: {education.degree}</p>
+          </div>
+        );
+        break;
 
-        switch(education.education_type){
-            case 'Tertiary':
-                educationTypeContent = (
-                    <div className={classes.content} >
-                            <h2>{education.end_year ? `${education.start_year} - ${education.end_year}` : "Till Date"}</h2>
-                            <p>Institution: {education.institution}</p>
-                            <p>Course of study: {education.course}</p>
-                            <p>Degree Obtained: {education.degree}</p>
-                            <p>Class of Degree: {education.class_of_degree}</p>
-                    </div>
-                ); break;
-    
-            case 'College': 
-                educationTypeContent = (
-                    <div className={classes.content} >
-                            <h2>{education.end_year ? `${education.start_year} - ${education.end_year}` : 
-                            `${education.start_year} - Till Date`}</h2>
-                            <p>Institution: {education.institution}</p>
-                            <p>Degree Obtained: {education.degree}</p>
-                    </div>
-                ); break;
-
-            default: educationTypeContent = null;
-        }
-
-        return educationTypeContent
+      default:
+        educationTypeContent = null;
     }
 
-    const sortedEducationHistory = [...props.educationHistory].sort((a,b) => b.end_year - a.end_year)
+    return educationTypeContent;
+  };
 
-    return (
-        <div className={`${props.wrapper}`}>
-            <p className={classes.title}>{props.title}</p>
-            <div className={classes.timeline}>
-                {sortedEducationHistory.map((education, index) => 
-                    <div className={`${classes.container}  ${direction}`}key={index}>
-                        {display(education)}
-                        <p hidden>{direction===classes.left ? direction=classes.right : direction=classes.left}</p>
-                    </div> 
-                )}
-            </div>
-        </div>
-    )
-}
+  const sortedEducationHistory = [...props.educationHistory].sort(
+    (a, b) => b.start_year - a.start_year
+  );
 
-const mapStateToProps = state =>{
-    return{
-        educationHistory: state.education
-    }
-}
+  return (
+    <div className={`${props.wrapper}`}>
+      <p className={classes.title}>{props.title}</p>
+      <div className={classes.timeline}>
+        {sortedEducationHistory.map((education, index) => (
+          <div className={`${classes.container}  ${direction}`} key={index}>
+            {display(education)}
+            <p hidden>
+              {direction === classes.left
+                ? (direction = classes.right)
+                : (direction = classes.left)}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(Education)
+const mapStateToProps = (state) => {
+  return {
+    educationHistory: state.education,
+  };
+};
+
+export default connect(mapStateToProps)(Education);
