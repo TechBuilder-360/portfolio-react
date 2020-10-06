21349feb7838d8885a14ad1a9be73458017e55dc
folayarn <folayanshola@gmail.com>
2020-10-06 10:24:40 +0100

    changes

--- src/components/profile/projects/projects.js
+++ src/components/profile/projects/projects.js
@@ -3,26 +3,43 @@ import { connect } from "react-redux";
 import {Link} from 'react-router-dom'
 import classes from "./projects.module.css";
 
+
+function text_truncate(str,len,ending){
+  if(len == null){
+    len=50
+  }
+if(ending == null){
+ending= '...'
+}
+if(str.length > len){
+  return str.substring(0,len-ending.length)+ending;
+}else{
+  return str 
+}
+}
+
 const Projects = (props) => {
   return (
     <div className={props.wrapper}>
-      <p className="title" style={{textAlign: "left"}}>Projects</p>
+      <p className={classes.title}>Projects</p>
       <div className={classes.container}>
         {props.projects.map((project, index) => (
           <div key={index} className={classes.card}>
-            <div className={classes.head} title={project.title}>{project.title}</div>
+            <div className={classes.head}>{project.title}</div>
             <div className={classes.body}>
-              {project.description}
-
+            <span>
+              {text_truncate(project.description)}<br/>
+              </span>
               <Link to="#" className={classes.link}>
                 {" "}
-                More...
+                Read More
               </Link>
             </div>
           </div>
         ))}
       </div>
     </div>
+    
   );
 };
 
