86fadbbdde0ad1b15be1da942402b91ed19174af
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-11-08 17:58:56 +0100

    resolved issue with accordion

--- src/components/portfolio-edit/education/components/child.js
+++ src/components/portfolio-edit/education/components/child.js
@@ -1,50 +1,38 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
+import EducationForm from "./educationForm";
+import { Accordion, Card, useAccordionToggle } from "react-bootstrap";
 import classes from "../../personal_info/personalInfo.module.css";
-import SubEducation from "./educationForm";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {Accordion,Card, useAccordionToggle} from "react-bootstrap"
-import {
-  faAngleDown,
-  faAngleUp,
-  faTrash,
-} from "@fortawesome/free-solid-svg-icons";
 
-function CustomToggle({children,eventKey}){
-const decoratedOnClick=useAccordionToggle(eventKey,()=>
-console.log(eventKey)
-)
-return(
-<a type="button" onClick={decoratedOnClick}> {children}</a>
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionToggle(eventKey, null) // Add checker to see if form has been edited and needs saving before closing
 
-)
-
+  return (
+    <a id={`close-accordion-${eventKey}`} onClick={decoratedOnClick}>
+      {" "}
+      {children}
+    </a>
+  );
 }
 
+const Child = ({ education, ...props }) => {
 
-const Child = (props) => {
   return (
-    <div style={{paddingBottom:'15px'}}>
-      <Card >
-        <Card.Header>
-        Organization
-          <span style={{float: "Right"}}>
-          <CustomToggle eventKey={props.i}>edit</CustomToggle>|
-          <a type="button" onClick={props.removeMore}>delete</a>
-          </span>
-        </Card.Header>
-        <Accordion.Collapse eventKey={props.i}>
-       <Card.Body><SubEducation submithandler={props.submithandler} /></Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    
-     
-
-
-
-      
-      
-      </div>
-    
+    <Card className={classes.Accordion_Child}>
+      <Card.Header>
+        {education.institution}
+        <span style={{ float: "Right" }}>
+          <CustomToggle eventKey={props.i}>Edit</CustomToggle> |{" "}
+          <a type="button" onClick={props.removeMore}>
+            Delete
+          </a>
+        </span>
+      </Card.Header>
+      <Accordion.Collapse eventKey={props.i}>
+        <Card.Body>
+          <EducationForm closeForm={()=>document.getElementById(`close-accordion-${props.i}`).click()}/>
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>
   );
 };
 export default Child;
--- src/components/portfolio-edit/education/education.js
+++ src/components/portfolio-edit/education/education.js
@@ -1,41 +1,58 @@
-import React, { useState} from "react";
+import React, { useEffect, useState } from "react";
 import style from "../profile-edit.module.css";
-import classes from "../personal_info/personalInfo.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-import {Accordion} from "react-bootstrap"
+import { Accordion, Fade } from "react-bootstrap";
 import Child from "./components/child";
+import { connect } from "react-redux";
+import classes from "../personal_info/personalInfo.module.css";
+import EducationForm from "./components/educationForm"
 
 
-const Education = () => {
-  const [numchild, setNumchild] = useState(0);
-  const children = [];
+const Education = (props) => {
 
-  let addMore =() => {
-    setNumchild(numchild + 1)
+  const [form, setForm] = useState(null)
+  const [formVisible, setFormVisible] = useState(false)
+
+  useEffect(() => {
+    if(formVisible){
+      setForm(<EducationForm closeForm={()=>setFormVisible(false)}/>)
+    }else{
+      setForm(null)
+    }
+  }, [formVisible]);
+
+  function removeMore(index) { // rethink function
   }
 
-  function removeMore(index){
-    const list = [...children];
-    list.splice(index, 1);
-    setNumchild(list.length);
-  }
-
-  for (var i = 0; i < numchild; i += 1) {
-    children.push(<Child removeMore={removeMore} i={i} key={i}/>);
-  }
+  // Populate accordion children with existing record
+  const children = props.education.map((edu, i) => (
+    <Child
+      education={edu}
+      removeMore={removeMore}
+      closeForm={()=>setFormVisible(false)}
+      i={i+1}
+      key={i}
+    />
+  ));
 
   return (
     <div className={style.SubSection}>
       <p className="title">Education</p>
       <hr />
-      <Accordion defaultActiveKey="0">
-      {children}
-      </Accordion>
-        <span onClick={addMore}>
-        <FontAwesomeIcon icon={faPlusCircle} size="lg"/>{" "}add more Education
-        </span>
-      </div>
-  )
-}
-export default Education;
+      <Accordion className={classes.Accordion_Parent}>{children}</Accordion>
+      {form}
+      <span onClick={()=>setFormVisible(true)}>
+        <FontAwesomeIcon icon={faPlusCircle} size="lg" /> add more Education
+      </span>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    education: state.education,
+  };
+};
+
+export default connect(mapStateToProps)(Education);
