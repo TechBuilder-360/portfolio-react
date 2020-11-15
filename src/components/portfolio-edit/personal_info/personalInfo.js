6d143f1ede99bd5f2dc36b5e255d427742bdfe57
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-11-15 15:20:30 +0100

    Renamed profile image component

--- src/components/portfolio-edit/personal_info/personalInfo.js
+++ src/components/portfolio-edit/personal_info/personalInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row } from "react-bootstrap";
 import style from "../profile-edit.module.css";
-import Images from "./components/image";
+import Images from "./components/profile_image";
 import PersonalDetails from "./components/personalDetails";
 
 const personalInfo = (props) => {
