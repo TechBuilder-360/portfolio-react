6464f7f4354a53801ef5ffbeec9e40a445f54a95
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-11-15 16:04:44 +0100

    url update

--- src/App.js
+++ src/App.js
@@ -14,32 +14,30 @@ import PasswordReset from "./components/authentication/Password/PasswordReset";
 import PasswordChange from "./components/authentication/Password/PasswordChange";
 import Container from "./container/Container";
 import Logout from "./components/authentication/Logout/Logout";
-// import { useSelector, shallowEqual } from "react-redux";
+import { connect } from "react-redux";
 
-const App = () => {
-  // const authState = useSelector((state) => state.auth, shallowEqual);
-  // console.log(authState.token);
+const App = (props) => {
   
   //Protected routes
-  // let routes = (
-  //   <Switch>
-  //     <Route path="/profile/:username/edit" exact component={ProfileEdit} />
-  //     <Route path="/contact" exact component={Contact} />
-  //     <Route path="/logout" exact component={Logout} />
-  //     <Route path="/profile/:username" exact component={Dashboard} />
-  //     {/* <Route path="/" exact component={Dashboard} /> Lets keep a single url for the dashboard irrespective of the case. Just like Github */}
-  //   </Switch>
-  // );
-  let routes = []
+  let routes = (
+    <Switch>
+      <Route path="/profile/:username/edit" exact component={ProfileEdit}/>
+      <Route path="/contact" exact component={Contact} />
+      <Route path="/logout" exact component={Logout} />
+      <Route path="/profile/:username" exact component={Dashboard} />
+      <Route path="/:username" exact component={Dashboard} />
+      <Route path="*" component={NotFound} />
+    </Switch>
+  );
 
-  if (true) {
+  if (!props.isAuthenticated) {
     //unprotected routes
     routes = (
       <Switch style={{ paddingLeft: "0" }}>
-        <Route path="/profile/:username/edit" exact component={ProfileEdit} />{" "}
         {/* Remove url when authentication is ready */}
         <Route path="/password/reset" exact component={PasswordReset} />
         <Route path="/password/change" exact component={PasswordChange} />
+
         {/* url pattern https://xportfolio.com/profile/user101 for dashboard*/}
         <Route path="/profile/:username" exact component={Dashboard} />
         <Route path="/faqs" exact component={FAQs} />
@@ -47,7 +45,6 @@ const App = () => {
         <Route path="/contact" exact component={Contact} />
         <Route path="/about" exact component={About} />
         <Route path="/login" exact component={Login} />
-        <Route path="/logout" exact component={Logout} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/" exact component={Home} />
         <Route path="*" component={NotFound} />
@@ -58,4 +55,10 @@ const App = () => {
   return <Container>{routes}</Container>;
 };
 
-export default App;
+const mapStateToProps = state =>{
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps)(App);
