c9339b92f76b145e9c75a727e9e968ef7ebd2e62
Jubril Aminu <jubrilaminu304@gmail.com>
2020-11-19 21:11:30 +0100

    set up social contact edit

--- src/components/portfolio-edit/social/child.js
+++ src/components/portfolio-edit/social/child.js
@@ -1,53 +1,46 @@
 import React from "react";
+import SocialForm from "./socialForm";
+import { Accordion, Card, useAccordionToggle } from "react-bootstrap";
+// import classes from "../../personal_info/personalInfo.module.css";
 import classes from "../personal_info/personalInfo.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { Col, Form, Row } from "react-bootstrap";
 
-const Child = (props) => {
-  const socialNetworks = [
-    "Facebook",
-    "LinkedIn",
-    "Twitter",
-    "Instagram",
-    "Github",
-  ];
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionToggle(eventKey, null); // Add checker to see if form has been edited and needs saving before closing
 
-  let socialNetworkOptions = socialNetworks.map((item) => (
-    <option key={item}>{item}</option>
-  ));
   return (
-    <div className={classes.container}>
-      <div className={classes.drop}>
-        <Form>
-          <Row>
-            <Col xs={12} md={5} className={classes.Mb_5}>
-              <Form.Group>
-                <Form.Label>Select Social Network</Form.Label>
-                <Form.Control as="select" defaultValue={props.label} custom>
-                  {socialNetworkOptions}
-                </Form.Control>
-              </Form.Group>
-            </Col>
-            <Col xs={12} md={6} className={classes.Mb_5}>
-              <Form.Group>
-                <Form.Label>Link to your page</Form.Label>
-                <Form.Control value={props.link} />
-              </Form.Group>
-            </Col>
-            <Col xs={12} md={1}>
-              <Form.Group style={{ float: "right" }}>
-                <FontAwesomeIcon
-                  // className={classes.delete}
-                  onClick={() => props.remove(props.id)}
-                  icon={faTrash}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-        </Form>
-      </div>
-    </div>
+    <a id={`close-accordion-${eventKey}`} onClick={decoratedOnClick}>
+      {" "}
+      {children}
+    </a>
+  );
+}
+
+const Child = ({ label, link, ...props }) => {
+  return (
+    <Card className={classes.Accordion_Child}>
+      <Card.Header>
+        {label}
+        &nbsp;{"-"}&nbsp;
+        {link}
+        <span style={{ float: "Right" }}>
+          <CustomToggle eventKey={props.i}>Edit</CustomToggle> |{" "}
+          <a type="button" onClick={() => props.removeMore(label, link)}>
+            Delete
+          </a>
+        </span>
+      </Card.Header>
+      <Accordion.Collapse eventKey={props.i}>
+        <Card.Body>
+          <SocialForm
+            link={link}
+            label={label}
+            closeForm={() =>
+              document.getElementById(`close-accordion-${props.i}`).click()
+            }
+          />
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>
   );
 };
 export default Child;
--- src/components/portfolio-edit/social/social.js
+++ src/components/portfolio-edit/social/social.js
@@ -4,84 +4,56 @@ import classes from "../personal_info/personalInfo.module.css";
 import Child from "./child";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-import { Button, Col } from "react-bootstrap";
 // import { useSelector, shallowEqual } from "react-redux";
 import { connect } from "react-redux";
+import SocialForm from "./socialForm";
+import { Accordion } from "react-bootstrap";
 
 const Social = (props) => {
   // const authState = useSelector((state) => state.porfolio, shallowEqual);
-  let userLinks = props.socialLinks;
-  const [numchild, setNumchild] = useState(0);
-  const [sLinks, setsLinks] = useState([]); 
 
-  // let children = [];
-  // useEffect(() => {
-  //   children = userLinks.map((item, index) => (
-  //     <Child
-  //       label={item.label}
-  //       link={item.url}
-  //       remove={remove}
-  //       key={index}
-  //       id={index}
-  //     />
-  //   ));
-  //   console.log(children);
-  //   setsLinks(children);
-  //   setNumchild(children.length);
-  // }, [props.socialLinks]);
+  const [form, setForm] = useState(null);
+  const [formVisible, setFormVisible] = useState(false);
 
-  let remove = useCallback((index) => {
-    // let newList = sLinks.filter((item) => item.props.id !== index);
-    // setsLinks(newList);
-    // setNumchild(numchild - 1);
-    console.log('test vic', index, sLinks.length, sLinks)
-    let even = [];
-    for(var i = 0; i < sLinks.length; i++) {
-      
-      console.log('test bool', i,)
-      if (sLinks[i].props.id != index) even.push(sLinks[i]);
+  useEffect(() => {
+    if (formVisible) {
+      setForm(<SocialForm closeForm={() => setFormVisible(false)} />);
+    } else {
+      setForm(null);
     }
-    setsLinks(even);
+  }, [formVisible]);
 
-    // console.log('test', even)
-  }, [sLinks]);
-
-  let addMore = () => {
-    if (numchild <= process.env.REACT_APP_SOCIAL_LINKS_LIMIT) {
-      let newItem = <Child remove={remove} key={Math.random()} id={Math.random()} />;
-      setsLinks([...sLinks, newItem]);
-      setNumchild(numchild + 1);
-    }
+  let remove = (label, link) => {
+    console.log("item to be removed", label, link);
+    //Dispatch event to remove the parameter from redux
   };
-  
-  console.log("comps", sLinks);
+
+  // Populate accordion children with existing record
+  const children = props.socialLinks.map((item, i) => (
+    <Child
+      label={item.label}
+      link={item.url}
+      removeMore={remove}
+      closeForm={() => setFormVisible(false)}
+      i={i + 1}
+      key={i}
+    />
+  ));
+
   return (
     <div className={style.SubSection}>
       <p className="title">Social Contact</p>
-      <hr />
       <p className="sub-title">
         You can have up to {process.env.REACT_APP_SOCIAL_LINKS_LIMIT} social
         contact details
       </p>
-      <Col md={12}>
-        {sLinks}
-        <div style={{ marginTop: "30px" }}>
-          {numchild}
-          {numchild >= process.env.REACT_APP_SOCIAL_LINKS_LIMIT ? null : (
-            <span onClick={addMore}>
-              <FontAwesomeIcon icon={faPlusCircle} size="lg" /> add more social
-              contact
-            </span>
-          )}
-          <Button
-            style={{ float: "right" }}
-            type="submit"
-            className={classes.Mb_5}
-          >
-            Save
-          </Button>
-        </div>
-      </Col>
+      <hr />
+      <Accordion className={classes.Accordion_Parent}>{children}</Accordion>
+      {form}
+      <span onClick={() => setFormVisible(true)}>
+        <FontAwesomeIcon icon={faPlusCircle} size="lg" /> add more social
+        contact
+      </span>
     </div>
   );
 };
