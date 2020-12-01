f0c5fec4879ccc5ebee6edbe2bdd7a94daabe137
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-12-01 05:47:26 +0100

    adjusted indicator design

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -1,45 +1,14 @@
 import React from "react";
-
+import classes from './carousel.module.css'
 import "bootstrap/dist/css/bootstrap.css";
 import { Carousel } from "react-bootstrap";
-import img1 from "../../images/images.jpeg";
-import img2 from "../../images/images2.jpeg";
-import img3 from "../../images/portfolio.jpeg";
 import image from "../../components/Carousel/Carousel Image/carouselImage.jpg"
 import {faAward, faCog, faEnvelope,faPhone} from "@fortawesome/free-solid-svg-icons"
 
-import {fontAwesome, FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-
-       {/* <div style={{ width: "100%", height: "300px", backgroundColor: '#000000' }}>
-        <img className="d-block w-100" src={img1} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </div>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div style={{ width: "100%", height: "300px", backgroundColor: '#000000' }}>
-        <img className="d-block w-100" src={img2} alt="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </div>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div style={{ width: "100%", height: "300px", backgroundColor: '#000000' }}>
-        <img className="d-block w-100" src={img3} alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </div>  */}
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 const CarouselImage = () => {
   return (
-    <Carousel >
+    <Carousel className={classes.Carousel}>
     <Carousel.Item>
      <div className="container" style={{ width: "100", height: "806px",padding:"10px",fontFamily:"cursive"}}>
       <section style={{display:"flex",backgroundColor:"#000000",color:"#ffffff",padding:"15px"}}>
