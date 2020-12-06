2278fdc29a7784a3ca5dd1e2db3626a294e86b83
Hamzattech <ridwanulllah@gmail.com>
2020-12-06 20:09:21 +0100

    Box shadow fixed

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -9,34 +9,9 @@ import image from "../../components/Carousel/Carousel Image/carouselImage.jpg"
 import {faAward, faCog, faEnvelope,faPhone} from "@fortawesome/free-solid-svg-icons"
 
 import {fontAwesome, FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import styled from 'styled-components'
 
-       {/* <div style={{ width: "100%", height: "300px", backgroundColor: '#000000' }}>
-        <img className="d-block w-100" src={img1} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </div>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div style={{ width: "100%", height: "300px", backgroundColor: '#000000' }}>
-        <img className="d-block w-100" src={img2} alt="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </div>
-    </Carousel.Item>
-    <Carousel.Item>
-      <div style={{ width: "100%", height: "300px", backgroundColor: '#000000' }}>
-        <img className="d-block w-100" src={img3} alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </div>  */}
+
 const CarouselImage = () => {
   return (
     <Carousel >
