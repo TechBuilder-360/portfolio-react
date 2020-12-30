c923acb94f447131e691e7668ee535126719b673
Jubril Aminu <jubrilaminu304@gmail.com>
2020-12-30 11:19:33 +0100

    improved authLogout in reducer

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,7 +3,7 @@ import { Spinner } from "react-bootstrap";
 
 const SpinnerElement = () => {
     return (
-        <div>
+        <div style={{marginLeft: "50%"}}>
             <Spinner animation="border" style={{width: "150px", height: "150px", borderWidth: "10px"}} role="status">
                 <span className="sr-only">Loading...</span>
             </Spinner>
