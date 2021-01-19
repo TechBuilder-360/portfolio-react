3627aa4984129b66c9e40160d9899c9b41032ba2
Jubril Aminu <jubrilaminu304@gmail.com>
2021-01-19 12:58:59 +0100

    implements antd on social contact edit

--- src/components/portfolio-edit/social/social.js
+++ src/components/portfolio-edit/social/social.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect} from "react";
 import style from "../profile-edit.module.css";
-import classes from "../personal_info/personalInfo.module.css";
-import Child from "./child";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 import SocialForm from "./socialForm";
-import { Accordion } from "react-bootstrap";
+import { Collapse } from 'antd';
 
 const Social = () => {
   const limit = process.env.REACT_APP_SOCIAL_LINKS_LIMIT
   const socialLinks = useSelector((state) => state.portfolio.social);
+  const { Panel } = Collapse;
 
   const [form, setForm] = useState(null);
   const [formVisible, setFormVisible] = useState(false);
@@ -22,30 +21,32 @@ const Social = () => {
       setForm(null);
     }
   }, [formVisible]);
-
+  
   // Populate accordion children with existing record
-  const children = socialLinks.map((item, i) => (
-    <Child
-      label={item.label}
-      link={item.url}
-      id={item.id}
-      closeForm={() => setFormVisible(false)}
-      index={i + 1}
+  const panels = socialLinks.map((item, i) => (
+    <Panel
+      header={item.label}
+      showArrow={false}
       key={i}
-    />
+    >
+      <SocialForm link={item.url} label={item.label} id={item.id}/>
+    </Panel>
   ));
 
   return (
     <div className={style.SubSection}>
       <p className="title">Social Contact</p>
       <hr />
-      <Accordion className={classes.Accordion_Parent}>{children}</Accordion>
+      <Collapse accordion >
+        {panels}
+      </Collapse>
+      <hr/>
       {form}
       {socialLinks.length < limit ? 
       <span onClick={() => setFormVisible(true)}>
       <FontAwesomeIcon icon={faPlus} /> Add Social Contact
     </span>
-      : null}
+      : null} 
     </div>
   );
 };
