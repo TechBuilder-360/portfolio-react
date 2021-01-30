d4a74a82c053fdac35233c48fa818296d6b8d4d7
folayarn <folayanshola@gmail.com>
2021-01-30 22:51:23 +0100

    features and about us edit

--- src/components/pre-auth/About.js
+++ src/components/pre-auth/About.js
@@ -7,10 +7,26 @@ import Layout from '../../container/Layout/Layout';
 const About = () => {
     return (
         <Layout>
-            <p className='title'> About Us </p>
-            <p>Techbuilder 360 is a community of developers, coming together to learn and build skills by 
-                developing softwares that can be used to solve problems.</p>
-            <h5>What to know more about us</h5>
+            <h4> About Us </h4>
+            <p>TechBuilder 360 is a Team of Young and Talented Individuals who share a common Vision in learning and development of the World Technology. It was established on 4th July, 2020 when the world was battling
+                 with a pandemic which led to the team working remotely.</p>
+    <p>The team is opened to anyone from:
+   <em >
+    <ul>
+	<li>Web Development</li>
+	<li>Mobile Development</li>
+    <li>UI/UX/Product Design</li>
+	<li>DevOps</li>
+	<li>Digital Marketers/Promoters</li>
+    </ul>
+    </em>
+</p>
+
+                 <h4>Our Vision</h4>
+                 <p>To build a community of developers that would build a better future. </p>
+                 <br/>
+            <h5>WANT TO JOIN US?</h5>
+            <p>Send your Request to our Email highlighting your point of interest.</p>
             <p>
                 <FontAwesomeIcon icon={faEnvelope} /> 
                 <a target='blank' href="mailto:Tech.Builder.circle@gmail.com" className={classes.Link}> Tech.Builder.circle@gmail.com </a>
--- src/components/pre-auth/Features.js
+++ src/components/pre-auth/Features.js
@@ -4,12 +4,14 @@ import Layout from '../../container/Layout/Layout'
 const Features = () => {
     return (
         <Layout>
-            <p className='title'>Features</p>
+            <h4>Features</h4>
+            <p>This App comes with a lot of Features which include the following:</p>
             <ul>
                 <li>Create a portfolio.</li>
                 <li>Generate custom link to share portfolio.</li>
                 <li>Use custom theme for present portfolio.</li>
                 <li>Choose a prefered template for resume download.</li>
+                <li>Blogging system <span style={{color:'red'}}>Coming Soon!!</span></li>
             </ul>
         </Layout>
     );
