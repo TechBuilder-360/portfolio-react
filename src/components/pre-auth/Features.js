a64aae9b9fa049bb511c8731621bddb0cf08fe50
Jubril Aminu <jubrilaminu304@gmail.com>
2021-02-01 16:56:09 +0100

    modifies features and about content

--- src/components/pre-auth/Features.js
+++ src/components/pre-auth/Features.js
@@ -5,12 +5,11 @@ const Features = () => {
     return (
         <Layout>
             <h4>Features</h4>
-            <p>This App comes with a lot of Features which include the following:</p>
+            <p>This app comes with a lot of Features which include the following:</p>
             <ul>
                 <li>Create a portfolio.</li>
                 <li>Generate custom link to share portfolio.</li>
-                <li>Use custom theme for present portfolio.</li>
-                <li>Choose a prefered template for resume download.</li>
+                <li>Choose a preferred template for resume download or upload yours</li>
                 <li>Blogging system <span style={{color:'red'}}>Coming Soon!!</span></li>
             </ul>
         </Layout>
