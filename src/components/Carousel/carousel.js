418024533cea267bac9bfd85ec3f05032530121b
Jubril Aminu <jubrilaminu304@gmail.com>
2021-02-07 15:59:28 +0100

    fix carousel control bug

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 import classes from "./carousel.module.css"
 import img1 from "../../images/carousel-img1.jpg";
 import img2 from "../../images/carousel-img2.jpg";
 import img3 from "../../images/carousel-img3.jpg";
 
+
 const CarouselImage = () => {
+  const [sliderImage, setSliderImage] = useState(img1);
+
+  const switchImage = (id) => {
+    
+    switch(id){
+      case "1": setSliderImage(img1); break;
+      case "2": setSliderImage(img2); break;
+      case "3": setSliderImage(img3); break; 
+  
+      default: setSliderImage(img1)
+    }
+  };
+
+
   return (
     <div>
     <div className={classes.carousel}>
       <ul className={classes.slides}>
         <input type="radio" name="radio-buttons" id="img-1" checked />
         <li className={classes.slide_container}>
-          <div className={classes.slide_image}>
-            <img  alt="pic1" src={img1}/>
-          </div>
-        </li>
-        <input type="radio" name="radio-buttons" id="img-2" />
-        <li className={classes.slide_container}>
-          <div className={classes.slide_image}>
-            <img alt="pic2" src={img2}/>
-          </div>
-        </li>
-        <input type="radio" name="radio-buttons" id="img-3" />
-        <li className={classes.slide_container}>
-          <div className={classes.slide_image}>
-            <img alt="pic3" src={img3}/>
+          <div>
+            <img  className={classes.slide_image} alt="pic1" src={sliderImage}/>
           </div>
         </li>
       </ul>
@@ -34,9 +37,9 @@ const CarouselImage = () => {
           dolore magna aliqua.</p>
       </div>
       <div className={classes.carousel_dots}>
-          <label for="img-1" className={classes.carousel_dot} id="img-dot-1"></label>
-          <label for="img-2" className={classes.carousel_dot} id="img-dot-2"></label>
-          <label for="img-3" className={classes.carousel_dot} id="img-dot-3"></label>
+          <label onClick={() => switchImage("1")} className={classes.carousel_dot} ></label>
+          <label onClick={() => switchImage("2")} className={classes.carousel_dot} ></label>
+          <label onClick={() => switchImage("3")} className={classes.carousel_dot} ></label>
         </div>
     </div>
 </div>
