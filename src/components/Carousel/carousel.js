caea20815a727608ff99c2181ecd4b5f8e43631d
Jubril Aminu <jubrilaminu304@gmail.com>
2021-02-08 11:12:29 +0100

    carousel clean up complete

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -1,48 +1,46 @@
-import React, {useState} from "react";
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
 
 import classes from "./carousel.module.css"
 import img1 from "../../images/carousel-img1.jpg";
-import img2 from "../../images/carousel-img2.jpg";
+import img2 from "../../images/images2.jpeg";
 import img3 from "../../images/carousel-img3.jpg";
 
 
 const CarouselImage = () => {
-  const [sliderImage, setSliderImage] = useState(img1);
-
-  const switchImage = (id) => {
-    
-    switch(id){
-      case "1": setSliderImage(img1); break;
-      case "2": setSliderImage(img2); break;
-      case "3": setSliderImage(img3); break; 
-  
-      default: setSliderImage(img1)
-    }
-  };
-
 
   return (
-    <div>
     <div className={classes.carousel}>
-      <ul className={classes.slides}>
-        <input type="radio" name="radio-buttons" id="img-1" checked />
-        <li className={classes.slide_container}>
-          <div>
-            <img  className={classes.slide_image} alt="pic1" src={sliderImage}/>
-          </div>
-        </li>
-      </ul>
-      <div className={classes.carousel_text}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-          dolore magna aliqua.</p>
+      <div className={classes.carousel_slides}>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src={img1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src={img2}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={img3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
       </div>
-      <div className={classes.carousel_dots}>
-          <label onClick={() => switchImage("1")} className={classes.carousel_dot} ></label>
-          <label onClick={() => switchImage("2")} className={classes.carousel_dot} ></label>
-          <label onClick={() => switchImage("3")} className={classes.carousel_dot} ></label>
-        </div>
+      
+      <div className={classes.carousel_text}>
+        <p>"The world won't care about your self-esteem. The world will expect you to accomplish something BEFORE you feel good about yourself." - Bill Gates</p>
+      </div>
     </div>
-</div>
   )
 }
 
